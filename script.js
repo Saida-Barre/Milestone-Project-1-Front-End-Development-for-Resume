0e61744ff6aa5b38b85b0d5e521ff7e6d9d8792b
unknown <sbarre33@gmail.com>
2020-07-15 19:17:53 -0700

    wired up form using formData object and added eventListener for button

---
+++ script.js
@@ -0,0 +1,7 @@
+const form=document.querySelector("form");
+console.log(form)
+form.addEventListener("submit", submit);
+function submit (event) { 
+    event.preventDefault();
+    let formData = new FormData(form)
+    console.log(formData.get("newTask")); //wired up form using formData object and added eventListener
