4d74484d9357184d37a19cda2cbbc2a8869d0b55
unknown <sbarre33@gmail.com>
2020-07-16 21:14:24 -0700

    updated contact form script. Ran the 3 inputs and removed event.preventDefault()

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 //wired up form using formData object and added eventListener
-const form=document.querySelector("form");
-console.log(form)
+const form = document.querySelector("form");
+
 form.addEventListener("submit", submit);
 function submit (event) { 
-    event.preventDefault();
-    let formData = new FormData(form)
-    console.log(formData.get("comment")); 
+    //event.preventDefault();
+    let formData = new FormData(form);
+    console.log(form);
+    console.log(formData.get("yourEmail"));
+    console.log(formData.get("messageSubject"));
+    console.log(formData.get("message"));
+}
+
+//now that the input variables and button works in form, we need to populate this info into mailto email.
+
